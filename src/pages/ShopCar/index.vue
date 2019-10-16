e864b49d8ee7f4986746c28d775843e0300faf83
tianxing <jackcaotianxing@163.com>
2019-10-16 17:26:19 +0800

    shopcar 完

--- src/pages/ShopCar/index.vue
+++ src/pages/ShopCar/index.vue
@@ -7,22 +7,24 @@
     <div class="shopCar-safe">
       <ul>
         <li>
-          <span class="iconfont">&#xe77c;</span>
+          <span class="iconfont circle">&#xe77c;</span>
           <span>30天无忧退货</span>
         </li>
         <li>
-          <span class="iconfont">&#xe77c;</span>
+          <span class="iconfont circle">&#xe77c;</span>
           <span>48小时快速退款</span>
         </li>
         <li>
-          <span class="iconfont">&#xe77c;</span>
+          <span class="iconfont circle">&#xe77c;</span>
           <span>满88元免邮费</span>
         </li>
       </ul>
     </div>
     <div class="shopCar-content">
       <div class="emptyShop">
-        <button>登录</button>
+        <img src="./img/shopCar.png" alt="">
+        <span class="font">去添点什么吧</span>
+        <div class="btn">登录</div>
       </div>
     </div>
   </div>
@@ -43,17 +45,71 @@ export default {};
   -moz-osx-font-smoothing grayscale
   color #666
 .shopCar-container
-  height 100%
+  height 542px
   width 100%
   .shopCar-header
     width 100%
     height 40px
     line-height 40px
-    background-color red
+    background-color white
     padding 0 15px
     position relative
+    box-sizing border-box
     .posh
       position absolute
       right 15px
-
+      color red
+  .shopCar-safe
+    width 100%
+    height 35px
+    background-color #EEEEEE 
+    ul
+      width 100%
+      height 100%
+      display flex
+      li
+        width 100%
+        height 35px
+        flex 1
+        line-height 35px
+        margin-left 15px 
+        position relative
+        span 
+          color #666
+          font-size 12px
+        .circle
+          position absolute
+          left -10px
+          top 2px
+  .shopCar-content
+    width 100%
+    height 100%
+    background-color #EEEEEE
+    position relative
+    .emptyShop
+      // position absolute
+      // left 0
+      // top 0
+      // right 0
+      // bottom 0
+      // margin auto 
+      display flex
+      flex-direction column
+      align-items center
+      text-align center
+      img 
+        display block
+        width 210px
+        height 210px
+        margin-top 110px
+      .font
+        color #666666
+      .btn
+        width 210px
+        height 40px
+        line-height 40px
+        text-align center
+        background-color #B4282D
+        color white
+        margin-top 10px
 </style>
