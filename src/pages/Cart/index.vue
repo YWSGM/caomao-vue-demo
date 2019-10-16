e5b317c2c3b3789029a9e9a566c0928b064a3c4b
tianxing <jackcaotianxing@163.com>
2019-10-16 21:23:54 +0800

    cart组价的搭建完成

--- src/pages/Cart/index.vue
+++ src/pages/Cart/index.vue
@@ -21,16 +21,56 @@
         </li>
       </ul>
     </div>
+    <div class="discounts">
+      <p class="car">全场赠券</p>
+      <span class="text">再购100元立享满100元送优惠券</span>
+      <span class="right">去凑单 ></span>
+    </div>
     <div class="shopCar-content">
-      <ul>
-        <li>
-          <img src="" alt="">
-          <span>白胖子 温润净颜卸妆水</span>
-          <span>400ml</span>
+      <ul class="shopList">
+        <li class="shopItem">
+          <div class="circle"></div>
+          <img src="./imgs/01.jpg" alt />
+          <div class="selectionsText">
+            <p class="name">白胖子 温润净颜卸妆水</p>
+            <p class="specification">400ml ></p>
+            <p class="price">￥59.5</p>
+          </div>
+          <div class="CartControl"></div>
+        </li>
+        <li class="shopItem">
+          <div class="circle"></div>
+          <img src="./imgs/01.jpg" alt />
+          <div class="selectionsText">
+            <p class="name">白胖子 温润净颜卸妆水</p>
+            <p class="specification">400ml ></p>
+            <p class="price">￥59.5</p>
+          </div>
+          <div class="CartControl"></div>
+        </li>
+        <li class="shopItem">
+          <div class="circle"></div>
+          <img src="./imgs/01.jpg" alt />
+          <div class="selectionsText">
+            <p class="name">白胖子 温润净颜卸妆水</p>
+            <p class="specification">400ml ></p>
+            <p class="price">￥59.5</p>
+          </div>
+          <div class="CartControl"></div>
+        </li>
+        <li class="shopItem">
+          <div class="circle"></div>
+          <img src="./imgs/01.jpg" alt />
+          <div class="selectionsText">
+            <p class="name">白胖子 温润净颜卸妆水</p>
+            <p class="specification">400ml ></p>
+            <p class="price">￥59.5</p>
+          </div>
+          <div class="CartControl"></div>
         </li>
       </ul>
     </div>
-    <Footer/>
+    <Footer />
   </div>
 </template>
 
@@ -41,8 +81,8 @@ export default {};
 <style lang="stylus" scoped>
 @font-face
   font-family 'iconfont' /* project id 1461072 */
-  src url('//at.alicdn.com/t/font_1461072_ktewh15ma9.eot')
-  src url('//at.alicdn.com/t/font_1461072_ktewh15ma9.eot?#iefix') format('embedded-opentype'), url('//at.alicdn.com/t/font_1461072_ktewh15ma9.woff2') format('woff2'), url('//at.alicdn.com/t/font_1461072_ktewh15ma9.woff') format('woff'), url('//at.alicdn.com/t/font_1461072_ktewh15ma9.ttf') format('truetype'), url('//at.alicdn.com/t/font_1461072_ktewh15ma9.svg#iconfont') format('svg')
+  src url('//at.alicdn.com/t/font_1461072_7w1909sptgx.eot')
+  src url('//at.alicdn.com/t/font_1461072_7w1909sptgx.eot?#iefix') format('embedded-opentype'), url('//at.alicdn.com/t/font_1461072_7w1909sptgx.woff2') format('woff2'), url('//at.alicdn.com/t/font_1461072_7w1909sptgx.woff') format('woff'), url('//at.alicdn.com/t/font_1461072_7w1909sptgx.ttf') format('truetype'), url('//at.alicdn.com/t/font_1461072_7w1909sptgx.svg#iconfont') format('svg')
 .iconfont
   font-family 'iconfont' !important
   font-size 20px
@@ -91,4 +131,75 @@ export default {};
         .circle
           position absolute
           left -10px
-          top 2px</style>
+          top 2px
+  .discounts
+    width 100%
+    height 40px
+    display flex
+    align-items center
+    font-size 14px
+    .car
+      width 60px
+      height 14px
+      background-color red
+      border-radius 3px
+      line-height 14px
+      text-align center
+      font-size 12px
+      margin-left 12px
+      color white
+      font-weight 700
+    .text
+      margin 0 10px
+    .right
+      color red
+      margin-left 10px
+.shopCar-content
+  width 100%
+  min-height 700px
+  background-color #EEEEEE
+  .shopList
+    width 100%
+    height 100%
+    border 1px solid #eeeeee
+    box-sizing border-box
+    
+    .shopItem
+      width 100%
+      height 140px
+      background-color white
+      margin 10px 0
+      display flex
+      align-items center
+      .circle
+        width 16px
+        height 16px
+        border 2px solid #eeeeee
+        border-radius 50%
+        margin 0 10px
+      img
+        width 90px
+        height 90px
+        margin-right 8px
+      .selectionsText
+        height 90px 
+        font-size 16px
+        .name
+          width 100%
+          height 20px
+          margin-bottom 8px
+        .specification
+          background-color #FAFAFA 
+          margin-left 5px
+          height 20px
+          line-height 20px
+          display inline
+        .price
+          margin-top 27px
+          
+</style>
+  
+
+     
+    
+
