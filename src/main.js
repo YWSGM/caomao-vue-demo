50f8d8fee344675709ef8c1b533c94e87b31f0cd
wangli <1821180991@qq.com>
2019-10-16 14:59:58 +0800

    头部

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
-import Vue from 'vue';
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 引入Header
+import Header from './components/Header/Header.vue'
 Vue.config.productionTip = false
+
+// 注册成全局组件(公共组件)
+Vue.component(Header.name, Header)
+
 
 new Vue({
 	el:'#app',
