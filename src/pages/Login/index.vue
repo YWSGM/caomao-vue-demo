2f6990ec10edac9f994c7c8559241cd85909953b
zaqmjuop <1060007045@qq.com>
2019-10-16 17:39:25 +0800

    添加flex.styl, login-icons

--- src/pages/Login/index.vue
+++ src/pages/Login/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div>lifdddd</div>
+  <div class="container">
+    <MtHeader class="down-banner"></MtHeader>
+    <MtHeader></MtHeader>
+  </div>
 </template>
 
 <script>
+import { Header } from "mint-ui";
 export default {
-
-}
+  components: {
+    MtHeader: Header
+  }
+};
 </script>
 
-<style>
+<style lang="stylus" scoped>
+.container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #eee;
 
+  .down-banner {
+    background-color: #333;
+  }
+}
 </style>
