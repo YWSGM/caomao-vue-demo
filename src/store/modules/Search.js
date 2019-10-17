7e18b680cf10c373b1c01a79f8afda615b283e38
zhangqian <iconqian@163.com>
2019-10-17 18:42:43 +0800

    增加moudlue文件夹

---
+++ src/store/modules/Search.js
@@ -0,0 +1,16 @@
+// 引入接口api
+// import {reqClassfiy,reqHomelist,reqSearchlist,reqGoods} from '../../api'
+
+const state ={
+
+}
+
+const muations = {
+
+}
+
+
+export default {
+	state,
+	muations
+}
