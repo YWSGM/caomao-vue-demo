50a7b4367a01a2f65c8630f7adc1506a271ea08b
wangli <1821180991@qq.com>
2019-10-17 18:44:45 +0800

    修改

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import router from './router'
 // 引入store
 import store from './store'
-import VueTouch from 'vue-touch'
+
 import 'mint-ui/lib/style.css'
 import { Swipe, SwipeItem } from 'mint-ui';
 
@@ -21,10 +21,6 @@ Vue.config.productionTip = false
 Vue.component(Header.name, Header)
 // 注册成全局组件(公共组件)
 Vue.component(Footer.name, Footer)
-Vue.use(VueTouch, {name: 'v-touch'})
-VueTouch.config.swipe = {
-	threshold: 100 //手指左右滑动距离
-},
 
 new Vue({
 	el: '#app',
