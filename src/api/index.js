95af4747228f45544ce1dc4cf081f48ed527d21a
tianxing <jackcaotianxing@163.com>
2019-10-17 17:18:55 +0800

    数据与接口

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,10 @@
 import ajax from './ajax'
 const BAES = '/api'
-
-export const reqHome = ()=>ajax.get(BAES + `/home`)
+//获取首页分类
+export const reqHomelist = (id)=>ajax.get(BAES+`/homelist?id=${id}`)
+//获取识物页列表
+export const reqSearchlist = (id)=>ajax.get(BAES+`/search?id=${id}`)
+//获取分类页分类
+export const reqClassfiy = (id)=>ajax.get(BAES + `/classify?id=${id}`)
+//获取商品
+export const reqGoods = (id)=>ajax.get(BAES+`/goods?id=${id}`)
