fbab0b75f999b653f6656bc5315bbd2cef6b5a2e
zaqmjuop <1060007045@qq.com>
2019-10-18 13:52:53 +0800

    添加接口loginWithCode

--- mpvue-server/server.js
+++ mpvue-server/server.js
@@ -57,7 +57,8 @@ koaRouter.post("/loginWithPassword", async (ctx, next) => {
   if (typeof data.phone === "string" && typeof data.password === "string") {
     const user = datas.users.find(user => user.phone === data.phone);
     if (user && user.password === data.password) {
-      result = { code: 0, user };
+      const token = "this is token";
+      result = { code: 0, user, token };
     }
   }
   if (!result) {
@@ -67,12 +68,31 @@ koaRouter.post("/loginWithPassword", async (ctx, next) => {
 });
 koaRouter.post("/loginWithEmail", async (ctx, next) => {
   const data = ctx.request.body.data;
-  console.log(data)
   let result;
   if (typeof data.email === "string" && typeof data.password === "string") {
     const user = datas.users.find(user => user.email === data.email);
     if (user && user.password === data.password) {
-      result = { code: 0, user };
+      const token = "this is token";
+      result = { code: 0, user, token };
+    }
+  }
+  if (!result) {
+    result = { code: 1, message: "not user match" };
+  }
+  ctx.body = result;
+});
+koaRouter.post("/loginWithCode", async (ctx, next) => {
+  const data = ctx.request.body.data;
+  let result;
+  if (typeof data.phone === "string" && typeof data.code === "string") {
+    if (data.code === "666666") {
+      const user = datas.users.find(user => user.phone === data.phone);
+      if (user) {
+        const token = "this is token";
+        result = { code: 0, user, token };
+      }
+    } else {
+      result = { code: 1, message: "code wrong" };
     }
   }
   if (!result) {
